827ff6e0ced73ef46ee9898da5831a468762f023
derrod <rodney@rodney.io>
2026-02-28 00:45:32 +0100

    frontend: Add Y410 and R10l formats to settings (Windows only)

--- frontend/widgets/OBSBasic.cpp
+++ frontend/widgets/OBSBasic.cpp
@@ -1453,6 +1453,17 @@ static inline enum video_format GetVideoFormatFromName(const char *name)
 		return VIDEO_FORMAT_P216;
 	else if (astrcmpi(name, "P416") == 0)
 		return VIDEO_FORMAT_P416;
+#ifdef _WIN32
+}
+else if (astrcmpi(name, "Y410") == 0)
+{
+	return VIDEO_FORMAT_Y410;
+}
+else if (astrcmpi(name, "R10l") == 0)
+{
+	return VIDEO_FORMAT_R10L;
+#endif
+}
 #if 0 //currently unsupported
 	else if (astrcmpi(name, "YVYU") == 0)
 		return VIDEO_FORMAT_YVYU;
@@ -1461,8 +1472,7 @@ static inline enum video_format GetVideoFormatFromName(const char *name)
 	else if (astrcmpi(name, "UYVY") == 0)
 		return VIDEO_FORMAT_UYVY;
 #endif
-	else
-		return VIDEO_FORMAT_BGRA;
+else return VIDEO_FORMAT_BGRA;
 }
 
 static inline enum video_colorspace GetVideoColorSpaceFromName(const char *name)
